4ebe6136010f59573754d4337b300b62a74f014f
Kiyaaa <dzakiyaastriani07@gmail.com>
2025-11-28 00:14:37 +0700

    tes

--- Lagitakost/resources/views/welcome.blade.php
+++ Lagitakost/resources/views/welcome.blade.php
@@ -218,7 +218,7 @@
 
     <!-- Main Content -->
     <div class="main-content">
-        <h1 class="welcome-title">elcome to<br>Lagita Kost</h1>
+        <h1 class="welcome-title"> come to<br>Lagita Kost</h1>
         <p class="welcome-subtitle">Comfort First, Price Friendly</p>
 
         <a href="{{ route('login') }}" class="cta-button">
