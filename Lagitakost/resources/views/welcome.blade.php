1fdd7b24faae15331d20d8f724d2dcd6c064c796
Kiyaaa <dzakiyaastriani07@gmail.com>
2025-11-28 02:59:57 +0700

    doneee

--- Lagitakost/resources/views/welcome.blade.php
+++ Lagitakost/resources/views/welcome.blade.php
@@ -218,7 +218,7 @@
 
     <!-- Main Content -->
     <div class="main-content">
-        <h1 class="welcome-title">Ki<br>Lagita Kost</h1>
+        <h1 class="welcome-title">Kiyaaaaa<br>Lagita Kost</h1>
         <p class="welcome-subtitle">Comfort First, Price Friendly</p>
 
         <a href="{{ route('login') }}" class="cta-button">
